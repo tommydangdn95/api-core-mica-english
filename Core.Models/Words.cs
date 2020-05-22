f3c67dd7c8703bc1f82a86b888cbb3417e87c617
Tran Thuy <thuy.dang@getz.co>
2020-05-22 22:07:39 +0700

    init project

---
+++ Core.Models/Words.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Core.Models
+{
+    public class Words
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public List<Words> Sysnonym { get; set; }
+        public string Mean { get; set; }
+    }
+}
